b797bad308153468243fdf2fcee9b50a565cad87
iLeoable <leo@crystalnetwork.us>
2014-05-25 20:11:02 -0500

    Update .gitignore

---
+++ platforms/android/ant-build/Discounter-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/loforney/PhoneGap/Discounter/platforms/android/ant-build/Discounter-debug-unaligned.apk : \
+/Users/loforney/PhoneGap/Discounter/platforms/android/ant-build/Discounter.ap_ \
+/Users/loforney/PhoneGap/Discounter/platforms/android/ant-build/classes.dex \
+/Users/loforney/PhoneGap/Discounter/platforms/android/CordovaLib/ant-build/classes.jar \
